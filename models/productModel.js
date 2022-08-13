f18373311fb53f72fc7e2d9c2b8507f6041d8461
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-13 06:43:41 +0100

    [uuid] was required using destructuring method, some functions was also written.

--- models/productModel.js
+++ models/productModel.js
@@ -1,11 +1,32 @@
 const products = require('../data/products')
+const {v4: uuidv4} = require('uuid')
 
+// * will display all the products from the database which is the json file
 function findAll() {
     return new Promise((resolve, reject) => {
         resolve(products)
     })
 }
 
+
+
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        // * get the one with the product [id]
+        const product = products.find((p) => p.id === id)
+        resolve(product)
+    })
+}
+
+
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        // * get the one with the product [id]
+        const product = products.find((p) => p.id === id)
+        resolve(product)
+    })
+}
 module.exports = {
-    findAll
+    findAll, 
+    findById
 }
