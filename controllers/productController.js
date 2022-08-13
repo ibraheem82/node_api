0df581febacfeacd40fa38a60f8c0da106b6daa0
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-13 08:51:31 +0100

    [updateProduct] was created to update each product by thier (id).

--- controllers/productController.js
+++ controllers/productController.js
@@ -33,7 +33,7 @@ async function getProduct(req, res, id) {
 
 //*  @desc Create a Product
 //*  @route POST /api/product/:id
-async function createProduct(req, res, id) {
+async function createProduct(req, res) {
     try {
       const body = await getPostData(req)
 
@@ -57,8 +57,43 @@ async function createProduct(req, res, id) {
     }
   }
 
+
+  //*  @desc Update a Product
+//*  @route PUT /api/product/:id
+async function updateProduct(req, res, id) {
+  try {
+
+    const product = await Product.findById(id)
+
+    if(!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      const body = await getPostData(req)
+
+      const {title, description, price} = JSON.parse(body)
+  
+        //  * will take what is coming from the parsed body.
+        const productData = {
+          title: title || product.title,
+          description: description || product.description,
+          price: price || product.price,
+      }
+        const updProduct = await Product.upaate(id, productData)
+          // ! [201] created
+        res.writeHead(201, { "Content-Type": "application/json" })
+        // ** will give us a new product
+        return res.end(JSON.stringify(updProduct))
+    }
+     
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
   getProduct,
-  createProduct
+  createProduct,
+  updateProduct
 };
