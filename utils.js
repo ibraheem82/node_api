92f37817fbf2c9c7dde9e8b946d443ccdf2116ae
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-08-13 07:15:50 +0100

    [fs] was imported, [writeDataToFile] will help us create product check comments.

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 const fs  = require('fs')
-
+// * function will write data to the file.
 function writeDataToFile(filename, content) {
     fs.writeFileSync(filename, JSON.stringify(content), 'utf-8', (err) => {
         if(err) {
